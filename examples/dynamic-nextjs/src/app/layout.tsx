eebafd72c6dfde8413cf094848d4202c6103f589
samfairbairn <samfairbairn@hotmail.co.uk>
2026-03-17 18:55:28 +0800

    update dyanmic example evm network

--- examples/dynamic-nextjs/src/app/layout.tsx
+++ examples/dynamic-nextjs/src/app/layout.tsx
@@ -4,7 +4,7 @@ import "@story-protocol/global-wallet/story";
 
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-// import { ZeroDevSmartWalletConnectors } from "@dynamic-labs/ethereum-aa";
+import { ZeroDevSmartWalletConnectors } from "@dynamic-labs/ethereum-aa";
 
 export default function RootLayout({
   children,
@@ -19,30 +19,24 @@ export default function RootLayout({
             environmentId:
               process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID ||
               "dbc1d91b-94dc-4762-b011-3a2a3a7357b7",
-            walletConnectors: [EthereumWalletConnectors],
+            // "17a2e296-2776-4d0f-bc8d-daaf5d4e0fde",
+            walletConnectors: [
+              EthereumWalletConnectors,
+              ZeroDevSmartWalletConnectors,
+            ],
             overrides: {
               evmNetworks: [
                 {
-                  blockExplorerUrls: ["https://storyscan.xyz"],
                   chainId: 1514,
-                  chainName: "Story",
-                  name: "Story",
-                  iconUrls: [],
-                  nativeCurrency: { decimals: 18, name: "IP", symbol: "IP" },
+                  blockExplorerUrls: ["https://storyscan.io"],
+                  iconUrls: [
+                    "https://app.dynamic.xyz/assets/networks/sepolia.svg",
+                  ],
+                  name: "story",
+                  nativeCurrency: { name: "Story", symbol: "IP", decimals: 18 },
                   networkId: 1514,
-                  rpcUrls: ["https://mainnet.storyrpc.io"],
-                  vanityName: "Story",
-                },
-                {
-                  blockExplorerUrls: ["https://testnet.storyscan.xyz"],
-                  chainId: 1513,
-                  chainName: "Story Testnet",
-                  name: "Story Testnet",
-                  iconUrls: [],
-                  nativeCurrency: { decimals: 18, name: "IP", symbol: "IP" },
-                  networkId: 1513,
-                  rpcUrls: ["https://testnet.storyrpc.io"],
-                  vanityName: "Story Testnet",
+                  rpcUrls: ["https://mainnet.storyrpc.io/"],
+                  vanityName: "Story Mainnet",
                 },
               ],
             },
